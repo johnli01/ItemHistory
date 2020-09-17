23ca3bccab51f9d67b4132d6969ffc4f2765b0a4
johnli01 <john.li9631@gmail.com>
2020-09-16 18:33:39 -0700

    Autonomizing GPU purchase from BestBuy

--- searcher.py
+++ searcher.py
@@ -1,46 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 import time
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe",
                           chrome_options=options)
-driver.get("https://www.reddit.com/r/hardwareswap/search/"
-           "?q=founders&source=recent&restrict_sr=1&sort=new")
+driver.get("https://www.bestbuy.com/site/apple-lightning-to-3-5mm-"
+           "headphone-adapter-white/5622278.p?skuId=5622278")
 
-recent = driver.find_elements_by_xpath("//*[contains(@class, 'y8HYJ-y_lTUHkQIc1mdCq _2INHSNB8V5eaWp4P0rY_mE')]")
+recent = driver.find_element_by_xpath("//*[contains(@class, "
+                                       "'btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button')]")
 
-key_word = 'Founders'
-filtered_list = []
-print("FILTERING")
-for listing in recent:
-    if key_word in listing.text:
-        filtered_list.append(listing)
+recent.click()
 
-filtered_list = filtered_list[1:]
-seen = set()
-print("MARKING SEEN")
-for listing in filtered_list:
-    seen.add(listing)
+time.sleep(1)
 
-print("RUNNING WHILE LOOP")
-iter = 1
-while True:
-    print("ITERATION : ", iter)
-    recent = driver.find_elements_by_xpath("//*[contains(@class, 'SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE')]")
-    unseen = []
-    for listing in recent:
-        if key_word in listing.text and listing not in seen:
-            unseen.append(listing)
-            seen.add(listing)
+recent = driver.find_element_by_xpath("//*[contains(@class, "
+                                       "'btn btn-secondary btn-sm btn-block ')]")
 
-    # for i in range(len(unseen)):
-    #     unseen[i].click()
-
-    if len(unseen) > 0:
-        unseen[0].click()
-    
-    time.sleep(3)
-    iter = iter + 1
-
-driver.quit()
+recent.click()
