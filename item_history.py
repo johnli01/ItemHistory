d10672da4816bde9d2c05ee5cdbfa26631b6d46e
johnli01 <john.li9631@gmail.com>
2021-09-28 17:10:51 -0700

    Pushing and merging progress

--- item_history.py
+++ item_history.py
@@ -8,14 +8,19 @@ import requests
 import time
 
 api = PushshiftAPI()
+transaction_counter = 0
 
-
+# Converts Epoch time To Standard format
 def epochToReg(time):
   return int(datetime.datetime.fromtimestamp(time))
 
+# Converts Standard format to Epoch time
 def regToEpoch(time):
   return int(datetime.datetime(time.year, time.month, time.day, 0, 0).timestamp())
 
+
+# Retrieve item data from subreddit within specified timeframe,
+# if timeframe not specified, defaults to 3 months in the past from now
 def getData(item, start=regToEpoch(date.today()), end=regToEpoch(date.today()+relativedelta(months=-3))):
   url = 'https://api.pushshift.io/reddit/search/submission/?before='+str(start)+"&after="+str(end)+"&subreddit=mechmarket"+"&q="+str(item)
   print(url)
@@ -28,14 +33,29 @@ def getData(item, start=regToEpoch(date.today()), end=regToEpoch(date.today()+re
   return data['data']
 
 
-def validData(data):
+# Filters out buyers of specified item and saves seller listings
+def validData(listings):
+  filtered = []
+  for listing in listings:
+    title = listing['title']
+    try:
+      if title[title.index("[H]"):title.index("[W]")]:
+        filtered.append(listing)
+    except ValueError:
+      continue
+  return filtered
 
+
+def getInfo(listing):
+  text = listing['selftext']
 
 
 def main():
   item = input("Enter specific item to search for on /r/MechMarket: ")
   data = getData(item)
   data = validData(data)
+  for listing in data:
+    print(listing)
 
 if __name__ == "__main__":
   main()
